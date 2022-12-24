5fcc8c2d7cf78896e66d7bc31ab48e1c6996c38b
jbordalo <j.bordalo@campus.fct.unl.pt>
2022-12-24 16:16:57 +0000

    Implement opcodes up to 0xC

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 pub const SCREEN_WIDTH: usize = 64;
 pub const SCREEN_HEIGHT: usize = 32;
 
@@ -81,21 +83,21 @@ impl Emulator {
         self.ram = [0; RAM_SIZE];
         self.address_register = 0;
         self.registers = [0; NUM_REGS];
-        self.display = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
         self.stack_pointer = 0;
         self.stack = [0; STACK_SIZE];
         self.keys = [false; NUM_KEYS];
         self.delay_timer = 0;
         self.sound_timer = 0;
 
+        self.clear_screen();
         self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
     }
 
     pub fn tick(&mut self) {
         // Fetch
         let instruction = self.fetch();
-        // Decode
-        // Execute
+        // Decode and Execute
+        self.execute(instruction);
     }
 
     fn fetch(&mut self) -> Opcode {
@@ -103,6 +105,103 @@ impl Emulator {
         let lower = self.ram[self.pc as usize + 1] as u16;
         self.pc += 2;
         (higher << 8) | lower
+    }
+
+    fn execute(&mut self, instruction: Opcode) {
+        // Match by digit
+        let digit3 = (instruction >> 12) & 0xF;
+        let digit2 = (instruction >> 8) & 0xF;
+        let digit1 = (instruction >> 4) & 0xF;
+        let digit0 = instruction & 0xF;
+
+        match (digit3, digit2, digit1, digit0) {
+            (0, 0, 0, 0) => (),
+            (0, 0, 0xE, 0) => self.clear_screen(),
+            (0, 0, 0xE, 0xE) => self.pc = self.pop(),
+            (1, _, _, _) => self.pc = instruction & 0xFFF,
+            (2, _, _, _) => {
+                self.push(self.pc);
+                self.pc = instruction & 0xFFF;
+            },
+            (3, register, _, _) => {
+                let nn = instruction & 0xFF;
+                if self.registers[register as usize] as Address == nn {
+                    self.pc += 2; // Skip one opcode
+                }
+            },
+            (4, register, _, _) => {
+                let nn = instruction & 0xFF;
+                if self.registers[register as usize] as Address != nn {
+                    self.pc += 2; // Skip one opcode
+                }
+            },
+            (5, x, y, 0) => {
+                if self.registers[x as usize] == self.registers[y as usize] {
+                    self.pc += 2; // Skip one opcode
+                }
+            },
+            (6, register, _, _) => {
+                let nn = instruction & 0xFF;
+                self.registers[register as usize] = nn as Register;
+            },
+            (7, register, _, _) => {
+                // Doesn't affect the carry flag 
+                let nn = instruction & 0xFF;
+                let sum = self.registers[register as usize].wrapping_add(nn as Register);
+                self.registers[register as usize] = sum;
+            },
+            (8, x, y, 0) => {
+                self.registers[x as usize] = self.registers[y as usize];
+            },
+            (8, x, y, 1) => {
+                self.registers[x as usize] |= self.registers[y as usize];
+            },
+            (8, x, y, 2) => {
+                self.registers[x as usize] &= self.registers[y as usize];
+            },
+            (8, x, y, 3) => {
+                self.registers[x as usize] ^= self.registers[y as usize];
+            },
+            (8, x, y, 4) => {
+                let (sum, carry) = self.registers[x as usize].overflowing_add(self.registers[y as usize]);
+                self.registers[x as usize] = sum;
+                self.registers[0xF] = if carry { 1 } else { 0 };
+            },
+            (8, x, y, 5) => {
+                let (sub, borrow) = self.registers[x as usize].overflowing_sub(self.registers[y as usize]);
+                self.registers[x as usize] = sub;
+                self.registers[0xF] = if borrow { 0 } else { 1 };
+            },
+            (8, x, _, 6) => {
+                let dropped_bit = self.registers[x as usize] & 0x1;
+                self.registers[x as usize] >>= 1;
+                self.registers[0xF] = dropped_bit;
+            },
+            (8, x, y, 7) => {
+                let (sub, borrow) = self.registers[y as usize].overflowing_sub(self.registers[x as usize]);
+                self.registers[x as usize] = sub;
+                self.registers[0xF] = if borrow { 0 } else { 1 };
+            },
+            (8, x, _, 0xE) => {
+                let dropped_bit = self.registers[x as usize] >> 7 & 0x1;
+                self.registers[x as usize] <<= 1;
+                self.registers[0xF] = dropped_bit;
+            },
+            (9, x, y, 0) => {
+                if self.registers[x as usize] != self.registers[y as usize] { self.pc += 2 };
+            },
+            (0xA, _, _, _) => {
+                self.address_register = instruction & 0xFFF;
+            },
+            (0xB, _, _, _) => {
+                self.pc = self.registers[0] as Address + (instruction & 0xFFF);
+            },
+            (0xC, x, _, _) => {
+                let nn = (instruction & 0xFF) as u8;
+                self.registers[x as usize] = self.rand() & nn;
+            },
+            (_, _, _, _) => unimplemented!("Unimplemented opcode: {}!", instruction),
+        }
     }
 
     pub fn tick_timers(&mut self) {
@@ -115,6 +214,14 @@ impl Emulator {
             }
             self.sound_timer -= 1;
         };
+    }
+
+    fn rand(&self) -> Register {
+        rand::thread_rng().gen()
+    }
+
+    fn clear_screen(&mut self) {
+        self.display = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
     }
 
     fn push(&mut self, addr: Address) {
